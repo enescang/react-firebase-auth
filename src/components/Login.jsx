5c3e9b24571b14b88c9417cb479888162622c7af
kodcanlisi <developerindiee@gmail.com>
2020-12-30 15:10:18 +0300

    Basic Login with Firebase

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,6 +1,35 @@
+import { useState } from 'react';
+import { firebaseClient } from "../firebase/firebase-client";
+
 const Login = () => {
-    return(
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+
+    const loginUser = async (event) => {
+        event.preventDefault();
+        try {
+            const user = await firebaseClient.auth().signInWithEmailAndPassword(email, password)
+            alert(user.user.email)
+        } catch (error) {
+            alert(error);
+        }
+
+    }
+    return (
         <>
+            <form
+                onSubmit={loginUser}
+            >
+                <input
+                    onChange={(event) => setEmail(event.target.value)}
+                    type="text"
+                    placeholder="Enter email" />
+                <input
+                    onChange={(event) => setPassword(event.target.value)}
+                    type="text"
+                    placeholder="Enter password" />
+                <button type="submit">Login</button>
+            </form>
             <h1>Login Page</h1>
         </>
     )
