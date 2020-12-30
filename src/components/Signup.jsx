109bcacd432b6a3cfb10ff2e5c42dfd9ff40037f
kodcanlisi <developerindiee@gmail.com>
2020-12-30 15:09:59 +0300

    Basic signup with firebase

--- src/components/Signup.jsx
+++ src/components/Signup.jsx
@@ -1,6 +1,36 @@
+import { useState } from 'react';
+import { firebaseClient } from "../firebase/firebase-client";
+
 const Signup = () => {
-    return(
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+
+    const signupUser = async (event) => {
+        event.preventDefault();
+        try {
+            const user = await firebaseClient.auth().createUserWithEmailAndPassword(email, password)
+            alert(user.user.email)
+        } catch (error) {
+            alert(error);
+        }
+
+    };
+    return (
         <>
+
+            <form
+                onSubmit={signupUser}
+            >
+                <input
+                    onChange={(event) => setEmail(event.target.value)}
+                    type="text"
+                    placeholder="Enter email" />
+                <input
+                    onChange={(event) => setPassword(event.target.value)}
+                    type="text"
+                    placeholder="Enter password" />
+                <button type="submit">Signup</button>
+            </form>
             <h1>Signup Page</h1>
         </>
     )
