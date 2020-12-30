25aa979f7c2f6c899ceb58618071a65e15ab8685
kodcanlisi <developerindiee@gmail.com>
2020-12-30 16:29:29 +0300

    Add spinner to Initial.jsx

--- src/components/Initial.jsx
+++ src/components/Initial.jsx
@@ -1,6 +1,23 @@
+import Loader from 'react-loader-spinner'
 const Initial = () => {
     return (
-        <h>Page is loading...</h>
+        <>
+            <div className="container">
+                <div className="d-flex align-items-center justify-content-center" style={{ height: 560 }}>
+                    <div className="bd-highlight col-example">
+                        <Loader
+                            type="Bars"
+                            color="#F5820D"
+                            height={100}
+                            width={100}
+                            timeout={30000} //3 secs
+
+                        />
+                    </div>
+                </div>
+            </div>
+        </>
+
     )
 }
 
